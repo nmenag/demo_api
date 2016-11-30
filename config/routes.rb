5dc8e1b25aa603dfead59fcc6bb3785435cd06e8
Nicolas Mena <nmena.garzon@gmail.com>
2016-11-30 00:02:07 -0500

    Add versioning

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   resources :tasks
   devise_for :users
   resources :users, only: [:index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :tasks
+      resources :users, only: [:index, :show] do
+        resources :tasks, only: [:index]
+      end
+    end
+  end
 end
