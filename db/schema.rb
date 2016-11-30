e17065bff10e3948139b8cb5dc4baa4485858f0c
Nicolas Mena <nmena.garzon@gmail.com>
2016-11-29 23:16:18 -0500

    Add task resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130033416) do
+ActiveRecord::Schema.define(version: 20161130041429) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.boolean  "completed",   default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "", null: false
